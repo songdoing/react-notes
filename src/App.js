e52d2f67a7dd449002ab3bfc040f006ab1ffd7f6
wonjinsong <msmk22239@gmail.com>
2020-05-04 00:22:05 -0400

    isRequired

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name="Jenny">react</MyComponent>;
+  return <MyComponent name="Jenny" favoriteNumber={5}>react</MyComponent>;
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({name, children }) => {
+const MyComponent = ({name, favoriteNumber, children }) => {
     
     return (
-        <div> Hello. I'm {name}. <br/> The value of children is {children}</div>
+        <div> Hello. I'm {name}. <br/> The value of children is {children}. <br/> My favorite number is {favoriteNumber}.</div>
     );    
 };
 MyComponent.defaultProps = {
     name : 'react'
 };
 
-MyComponent.PropTypes = {
-    name : PropTypes.string
+MyComponent.propTypes = {
+    name : PropTypes.string,
+    favoriteNumber : PropTypes.number.isRequired
 };
 
 export default MyComponent;
