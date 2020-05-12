822b48e02eb797219ffe2a1247abb587db8f33a6
wonjinsong <msmk22239@gmail.com>
2020-05-12 01:47:12 -0400

    predefine event handler function

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -6,27 +6,52 @@ class EventPractice extends Component {
         message : ''
     }
 
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState ({
+            message : e.target.value
+        });
+    }
+
+    handleClick() {
+        alert(this.state.message);
+        this.setState({
+            message : ''
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1>Event</h1>
-                <input type = "text" name="message" placeholder="write something." 
+                <input type = "text" name="message" placeholder="write something." value = {this.state.message}
                         onChange = {
+                            this.handleChange
+                            /*
                             (e) => {
                                 console.log(e.target.value);
                                 this.setState({
                                     message : e.target.value
                                 })
                             }
+                            */
                         } 
                 />
                 <button onClick = {
+                    this.handleClick
+                    /*
                     () => {
                         alert(this.state.message);
                         this.setState({
                             message : ''
                         });
                     }
+                    */
                 }>Enter</button>
             </div>
         );
