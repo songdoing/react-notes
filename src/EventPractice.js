00bb369f5227f654a1900f78719c6f78ded1d863
wonjinsong <msmk22239@gmail.com>
2020-05-15 17:58:19 -0400

    event function style

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,73 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends Component {
-    
-    state = {
-        username : '',
-        message : ''
-    }
-/*
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
-*/
-    handleChange = (e) => {
-        this.setState ({
-            [e.target.name] : e.target.value
-        });
-    }
-
-    handleClick = () => {
-        alert('Hey  ' + this.state.username + '! ' + this.state.message);
-        this.setState({
-            username : '',
-            message : ''
-        });
-    }
-// when 'enter' key is pressed
-    handleKeyPress =(e) => {
-        if(e.key === 'Enter') {
-            this.handleClick();
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Event</h1>
-                <input type = "text" name="username" placeholder="username" value={this.state.username} onChange={this.handleChange} />
-                <input type = "text" name="message" placeholder="write something." value = {this.state.message}
-                        onChange = {
-                            this.handleChange
-                            /*
-                            (e) => {
-                                console.log(e.target.value);
-                                this.setState({
-                                    message : e.target.value
-                                })
-                            }
-                            */
-                        } 
-                        onKeyPress = {
-                            this.handleKeyPress
-                        }
-                />
-                <button onClick = {
-                    this.handleClick
-                    /*
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message : ''
-                        });
-                    }
-                    */
-                }>Enter</button>
-            </div>
-        );
-    }
-}
+const EventPractice = () => {
+    const [username, setUsername] = useState('');
+    const [message, setMessage] = useState('');
+    const onChangeUsername = e => setUsername(e.target.value);
+     const onChangeMessage = e => setMessage(e.target.value);
+     const onClick = () => {
+         alert ( username + ': ' + message);
+         setUsername('');
+         setMessage('');
+     };
+     const onKeyPress = e => {
+         if(e.key === 'Enter') {
+             onClick();
+         }
+     };
+     return (
+         <div>
+             <h1>Event</h1>
+             <input
+                type = "text"
+                name = "username"
+                placeholder = "username"
+                value = {username}
+                onChange = {onChangeUsername}
+            />
+            <input
+                type = "text"
+                name = "message"
+                placeholder = "message"
+                value = {message}
+                onChange = {onChangeMessage}
+                onKeyPress = {onKeyPress}
+            />
+            <button onClick={onClick}>Enter</button>
+         </div>
+     );
+};
 
 export default EventPractice;
