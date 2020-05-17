deeada81335e7fd69281533ca61478b81a009138
wonjinsong <msmk22239@gmail.com>
2020-05-17 16:20:49 -0400

    add doubleclick event for deleting

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -27,8 +27,14 @@ const IterationSample = () => {
         setInputText('');
         console.log(nextNames);
     }
+
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+        console.log(nextNames);
+    }
     
-    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const namesList = names.map(name => <li key={name.id} onDoubleClick = {() => onRemove(name.id)}>{name.text}</li>);
     return (
         <>
             <input value = {inputText} onChange = {onChange} onKeyPress={onKeyPress} />
