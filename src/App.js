c86e181784b54991e7a0c7cd6fbc6fa891a82ef6
wonjinsong <msmk22239@gmail.com>
2020-05-16 23:12:19 -0400

    add ref to component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-import ValidationSample from './ValidationSample';
-import './App.css';
+import ScrollBox from './ScrollBox';
+//import './App.css';
 
 class App extends Component {
   render() {
     return (
-    <ValidationSample></ValidationSample>
+      <div>
+        <ScrollBox ref = {(ref) => this.ScrollBox = ref}></ScrollBox>
+        <button onClick = { () => this.ScrollBox.scrollToBottom()}>To Bottom</button>
+        <button onClick = { () => this.ScrollBox.scrollToTop()}>To Top</button>
+      </div>    
     );
   }
 }
