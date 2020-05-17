018bf487e4a5554ed34b8a5678b5442ecb457d49
wonjinsong <msmk22239@gmail.com>
2020-05-17 16:06:29 -0400

    add input tag for creating

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-    const names = ['snowman', 'ice', 'snow', 'wind'];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-    return(
-        <ul>{nameList}</ul>
-    );
-    /*
+    const [names, setNames] = useState([
+        {id:1, text : 'snowman'},
+        {id:2, text : 'ice'},
+        {id:3, text : 'snow'},
+        {id:4, text : 'wind'}
+    ]);
+
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = e => setInputText(e.target.value);
+    const onKeyPress = e => {
+        if(e.key === 'Enter') {
+            onClick();
+        }
+    }
+    const onClick = () => {
+        const nextNames = names.concat({
+            id : nextId,
+            text : inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+        console.log(nextNames);
+    }
+    
+    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
     return (
-        <ul>
-            <li> snowman</li>
-            <li> ice</li>
-            <li> snow</li>
-            <li> wind</li>
-        </ul>
-    );
-    */
+        <>
+            <input value = {inputText} onChange = {onChange} onKeyPress={onKeyPress} />
+            <button onClick={onClick}>ADD</button>
+            <ul>{namesList}</ul>;
+        </>
+    );        
 };
 export default IterationSample;
