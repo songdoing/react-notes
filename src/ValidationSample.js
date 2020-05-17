fc214281a4ef813854117a93f89783cb289e3a25
wonjinsong <msmk22239@gmail.com>
2020-05-16 22:21:12 -0400

    add ref to input tag

--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -19,12 +19,16 @@ class ValidationSample extends Component {
             clicked : true,
             validated : this.state.password === '0000'
         });
+        this.input.focus();
+        this.setState ({
+            password : ''
+        });
     }
 
     render() {
         return(
             <div>
-                <input type = "password" value= {this.state.password}
+                <input ref = {(ref) => this.input = ref} type = "password" value= {this.state.password}
                  onChange={this.handleChange} className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''} />
                 <button onClick = {this.handleButtonClick}>Validation</button>
             </div>
